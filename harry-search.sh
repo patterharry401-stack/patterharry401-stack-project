738df5c6b46bdf40fa68bfc4a9b6b1c573cfd5ab
patterharry401-stack <patterharry401@gmail.com>
2026-02-11 07:49:23 +0700

    Auto Update: Wed Feb 11 07:49:23 +07 2026

--- harry-search.sh
+++ harry-search.sh
@@ -3,17 +3,25 @@ clear
 echo "=============================="
 echo "    HARRY-MAX OSINT SEARCH    "
 echo "=============================="
-read -p "Enter Target (Email or Username): " target
+echo "Master Harry, please enter a Username (e.g., patterharry401)"
+echo "OR an Email (e.g., example@gmail.com)"
+echo "------------------------------"
+read -p "Target: " target
+
+# ဆရာ မှားရိုက်တတ်တဲ့ စာသားတွေကို တားလိုက်ပြီ
+if [[ $target == *"update.sh"* ]] || [[ $target == *"./"* ]]; then
+    echo "!!! ERROR: ဆရာ... အဲ့ဒါ Command ကြီးလေ၊ နာမည်ပဲ ရိုက်ထည့်ပါဗျာ !!!"
+    exit
+fi
 
 if [[ $target == *"@"* ]]; then
-    echo "Searching Email: $target"
-    echo "------------------------------"
+    echo "Searching Email: $target..."
     holehe $target | grep "\[+\]"
 else
-    echo "Searching Username: $target"
-    echo "------------------------------"
-    # Sherlock လမ်းကြောင်းကို အသေချာဆုံးဖြစ်အောင် ရှာခိုင်းမယ်
-    python3 ~/patterharry401-stack-project/sherlock/sherlock.py $target --timeout 1 --print-found
+    echo "Searching Username: $target..."
+    # Sherlock ရှိတဲ့ နေရာအမှန်ကို အတင်းဝင်ခိုင်းမယ်
+    cd ~/patterharry401-stack-project/sherlock/sherlock_project/
+    python3 sherlock.py $target --timeout 1 --print-found
 fi
 
 echo "------------------------------"
