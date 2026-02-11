306f01c5c3436bd67075a551423f39052ef1c476
patterharry401-stack <patterharry401@gmail.com>
2026-02-11 08:11:38 +0700

    Auto Update: Wed Feb 11 08:11:38 +07 2026

--- harry-search.sh
+++ harry-search.sh
@@ -11,10 +11,10 @@ if [[ $target == *"@"* ]]; then
     holehe $target | grep "\[+\]"
 else
     echo "Searching Username: $target..."
-    # Sherlock ရဲ့ folder အဆင့်ဆင့်ထဲကို အတိအကျ ဝင်မယ်
-    cd ~/patterharry401-stack-project/sherlock/sherlock_project
-    # ဖိုင်ကို တိုက်ရိုက်မောင်းပြီး လိုအပ်တဲ့ Library တွေကို PYTHONPATH နဲ့ ချိတ်ပေးမယ်
-    PYTHONPATH=.. python3 sherlock.py $target --timeout 1 --print-found
+    # Sherlock ရဲ့ ပင်မ Folder ထဲကို ဝင်မယ်
+    cd ~/patterharry401-stack-project/sherlock
+    # Folder နာမည်ကို Python Module အနေနဲ့ လှမ်းခေါ်မယ် (ဒါမှ သူ မငြင်းတော့တာ)
+    python3 -m sherlock_project $target --timeout 1 --print-found
 fi
 
 echo "------------------------------"
